f8fc55cec56ba55749c7ec4c1ad16ccf4dce7c4c
Keisuke Yoshigai <yoshigai_keisuke@retail-ai.jp>
2023-03-04 09:21:14 +0900

    Improve 707 solution by adding length to linked list

--- 707.design-linked-list.py
+++ 707.design-linked-list.py
@@ -18,8 +18,12 @@ class Node:
 class MyLinkedList:
     def __init__(self):
         self.head = None
+        self.length = 0
 
     def get(self, index: int) -> int:
+        if index > self.length - 1 or index < 0:
+            return -1
+
         count = 0
         cur = self.head
         while cur is not None:
@@ -34,16 +38,19 @@ class MyLinkedList:
         node = Node(val=val, next=head)
         if head is None:
             self.head = node
+            self.length += 1
             return
 
         head.prev = node
         self.head = node
+        self.length += 1
 
     def addAtTail(self, val: int) -> None:
         node = Node(val=val)
         head = self.head
         if head is None:
             self.head = node
+            self.length += 1
             return
 
         cur = head
@@ -53,10 +60,14 @@ class MyLinkedList:
 
         node.prev = cur
         cur.next = node
+        self.length += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
         if index == 0:
             self.addAtHead(val)
+            return
+        if index == self.length:
+            self.addAtTail(val)
             return
 
         count = 1
@@ -71,6 +82,7 @@ class MyLinkedList:
                     added_node.next.prev = added_node
 
                 prev.next = added_node
+                self.length += 1
                 return
             prev = prev.next
             count += 1
@@ -82,6 +94,7 @@ class MyLinkedList:
                 self.head.prev = None
             else:
                 self.head = None
+            self.length -= 1
             return
 
         count = 1
@@ -93,9 +106,16 @@ class MyLinkedList:
                     cur = None
                 else:
                     cur.prev.next, cur.next.prev = cur.next, cur.prev
+                self.length -= 1
                 return
             cur = cur.next
             count += 1
+
+    def print(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.val)
+            cur = cur.next
 
     # Your MyLinkedList object will be instantiated and called as such:
     # obj = MyLinkedList()

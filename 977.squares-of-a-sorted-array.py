bc9340b58edace5d0ee6b618c15212984f313c66
Keisuke Yoshigai <yoshigai_keisuke@retail-ai.jp>
2023-03-05 07:24:55 +0900

    Add 141 solution(linked list cycle)

--- 977.squares-of-a-sorted-array.py
+++ 977.squares-of-a-sorted-array.py
@@ -4,14 +4,14 @@
 # [977] Squares of a Sorted Array
 #
 
+
 # @lc code=start
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
-            nums[i] = nums[i]*nums[i]
+            nums[i] = nums[i] * nums[i]
         nums.sort()
         return nums
 
 
 # @lc code=end
-
